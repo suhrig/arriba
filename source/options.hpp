e1e16780d2b7a29298a1e93afedede49428a20c4
Sebastian Uhrig <s.uhrig@dkfz.de>
2016-07-26 19:30:13 +0200

    Ariba 0.5

--- source/options.hpp
+++ source/options.hpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 const string HELP_CONTACT = "s.uhrig@dkfz.de";
-const string ARIBA_VERSION = "0.4";
+const string ARIBA_VERSION = "0.5";
 
 string wrap_help(const string option, const string text, const unsigned short int max_line_width = 80);
 
