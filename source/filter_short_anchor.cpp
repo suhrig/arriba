b4b7d19aac2fb58dd04b5891f34e6afd67d4476f
Sebastian Uhrig <s.uhrig@dkfz.de>
2016-12-24 23:17:28 +0100

    'short_anchor' filter does not remove spliced fusions

--- source/filter_short_anchor.cpp
+++ source/filter_short_anchor.cpp
@@ -6,14 +6,15 @@ using namespace std;
 
 unsigned int filter_short_anchor(fusions_t& fusions, unsigned int min_length) {
 	unsigned int remaining = 0;
-	for (fusions_t::iterator i = fusions.begin(); i != fusions.end(); ++i) {
+	for (fusions_t::iterator fusion = fusions.begin(); fusion != fusions.end(); ++fusion) {
 
-		if (!i->second.filters.empty())
+		if (!fusion->second.filters.empty())
 			continue; // fusion has already been filtered
 
-		if (abs(i->second.anchor_start1 - i->second.breakpoint1) < min_length ||
-		    abs(i->second.anchor_start2 - i->second.breakpoint2) < min_length) {
-			i->second.filters.insert(FILTERS.at("short_anchor"));
+		if (!(fusion->second.spliced1 && fusion->second.spliced2) &&
+		    (abs(fusion->second.anchor_start1 - fusion->second.breakpoint1) < min_length ||
+		     abs(fusion->second.anchor_start2 - fusion->second.breakpoint2) < min_length)) {
+			fusion->second.filters.insert(FILTERS.at("short_anchor"));
 		} else {
 			remaining++;
 		}
