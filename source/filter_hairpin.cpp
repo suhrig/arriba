213af4b6fcabebe88685463b94cf37dab54921b5
Sebastian Uhrig <s.uhrig@dkfz.de>
2017-01-05 23:11:27 +0100

    filter hairpin-mediated fusions even if the reads were assigned to different genes

--- source/filter_hairpin.cpp
+++ source/filter_hairpin.cpp
@@ -35,63 +35,68 @@ bool spliced_distance_too_short(const contig_t contig, const position_t breakpoi
 unsigned int filter_hairpin(chimeric_alignments_t& chimeric_alignments, exon_annotation_index_t& exon_annotation_index, const int max_mate_gap) {
 
 	unsigned int remaining = 0;
-	for (chimeric_alignments_t::iterator i = chimeric_alignments.begin(); i != chimeric_alignments.end(); ++i) {
+	for (chimeric_alignments_t::iterator chimeric_alignment = chimeric_alignments.begin(); chimeric_alignment != chimeric_alignments.end(); ++chimeric_alignment) {
 
-		if (i->second.filter != NULL)
+		if (chimeric_alignment->second.filter != NULL)
 			continue; // the read has already been filtered
 
-		// check if mate1 and mate2 map to the same gene
+		// check if mate1 and mate2 map to the same gene or close to one another
 		gene_set_t common_genes;
-		if (i->second.size() == 2) // discordant mate
-			combine_annotations(i->second[MATE1].genes, i->second[MATE2].genes, common_genes, false);
-		else // split read
-			combine_annotations(i->second[MATE2].genes, i->second[SUPPLEMENTARY].genes, common_genes, false);
-		if (common_genes.empty()) {
-			remaining++;
-			continue; // we are only interested in intragenic events here
+		if (chimeric_alignment->second.size() == 2) { // discordant mate
+			combine_annotations(chimeric_alignment->second[MATE1].genes, chimeric_alignment->second[MATE2].genes, common_genes, false);
+			if (common_genes.empty() && chimeric_alignment->second[MATE1].contig != chimeric_alignment->second[MATE2].contig) {
+				remaining++;
+				continue; // we are only interested in intragenic events
+			}
+		} else {// split read
+			combine_annotations(chimeric_alignment->second[SPLIT_READ].genes, chimeric_alignment->second[SUPPLEMENTARY].genes, common_genes, false);
+			if (common_genes.empty() && chimeric_alignment->second[SPLIT_READ].contig != chimeric_alignment->second[SUPPLEMENTARY].contig) {
+				remaining++;
+				continue; // we are only interested in intragenic events
+			}
 		}
 
-		float fragment_size = max_mate_gap + i->second[MATE1].sequence.length() * 2;
+		const float fragment_size = max_mate_gap + chimeric_alignment->second[MATE1].sequence.length() * 2;
 
-		if (i->second.size() == 2) { // discordant mates
+		if (chimeric_alignment->second.size() == 2) { // discordant mates
 
-			position_t breakpoint1 = (i->second[MATE1].strand == FORWARD) ? i->second[MATE1].end : i->second[MATE1].start;
-			position_t breakpoint2 = (i->second[MATE2].strand == FORWARD) ? i->second[MATE2].end : i->second[MATE2].start;
-			direction_t direction1 = (i->second[MATE1].strand == FORWARD) ? DOWNSTREAM : UPSTREAM;
-			direction_t direction2 = (i->second[MATE2].strand == FORWARD) ? DOWNSTREAM : UPSTREAM;
+			position_t breakpoint1 = (chimeric_alignment->second[MATE1].strand == FORWARD) ? chimeric_alignment->second[MATE1].end : chimeric_alignment->second[MATE1].start;
+			position_t breakpoint2 = (chimeric_alignment->second[MATE2].strand == FORWARD) ? chimeric_alignment->second[MATE2].end : chimeric_alignment->second[MATE2].start;
+			direction_t direction1 = (chimeric_alignment->second[MATE1].strand == FORWARD) ? DOWNSTREAM : UPSTREAM;
+			direction_t direction2 = (chimeric_alignment->second[MATE2].strand == FORWARD) ? DOWNSTREAM : UPSTREAM;
 
 			if (abs(breakpoint1 - breakpoint2) <= fragment_size ||
-			    spliced_distance_too_short(i->second[MATE1].contig, breakpoint1, breakpoint2, direction1, direction2, common_genes, fragment_size, exon_annotation_index) ||
-			    is_breakpoint_within_aligned_segment(breakpoint1, i->second[MATE2]) ||
-			    is_breakpoint_within_aligned_segment(breakpoint2, i->second[MATE1])) {
-				i->second.filter = FILTERS.at("hairpin");
+			    spliced_distance_too_short(chimeric_alignment->second[MATE1].contig, breakpoint1, breakpoint2, direction1, direction2, common_genes, fragment_size, exon_annotation_index) ||
+			    is_breakpoint_within_aligned_segment(breakpoint1, chimeric_alignment->second[MATE2]) ||
+			    is_breakpoint_within_aligned_segment(breakpoint2, chimeric_alignment->second[MATE1])) {
+				chimeric_alignment->second.filter = FILTERS.at("hairpin");
 				continue;
 			}
 
 		} else { // split read
 
-			position_t breakpoint_split_read = (i->second[SPLIT_READ].strand == FORWARD) ? i->second[SPLIT_READ].start : i->second[SPLIT_READ].end;
-			direction_t direction_split_read = (i->second[SPLIT_READ].strand == FORWARD) ? UPSTREAM : DOWNSTREAM;
-			position_t breakpoint_supplementary = (i->second[SUPPLEMENTARY].strand == FORWARD) ? i->second[SUPPLEMENTARY].end : i->second[SUPPLEMENTARY].start;
-			direction_t direction_supplementary = (i->second[SUPPLEMENTARY].strand == FORWARD) ? DOWNSTREAM : UPSTREAM;
-			position_t gap_start_mate1 = (i->second[MATE1].strand == FORWARD) ? i->second[MATE1].end : i->second[MATE1].start;
-			direction_t direction_gap_start_mate1 = (i->second[MATE1].strand == FORWARD) ? DOWNSTREAM : UPSTREAM;
-			position_t gap_start_split_read = (i->second[SPLIT_READ].strand == FORWARD) ? i->second[SPLIT_READ].end : i->second[SPLIT_READ].start;
-			direction_t direction_gap_start_split_read = (i->second[SPLIT_READ].strand == FORWARD) ? DOWNSTREAM : UPSTREAM;
-			position_t start_mate1 = (i->second[MATE1].strand == FORWARD) ? i->second[MATE1].start : i->second[MATE1].end;
-			direction_t direction_start_mate1 = (i->second[MATE1].strand == FORWARD) ? UPSTREAM : DOWNSTREAM;
+			position_t breakpoint_split_read = (chimeric_alignment->second[SPLIT_READ].strand == FORWARD) ? chimeric_alignment->second[SPLIT_READ].start : chimeric_alignment->second[SPLIT_READ].end;
+			direction_t direction_split_read = (chimeric_alignment->second[SPLIT_READ].strand == FORWARD) ? UPSTREAM : DOWNSTREAM;
+			position_t breakpoint_supplementary = (chimeric_alignment->second[SUPPLEMENTARY].strand == FORWARD) ? chimeric_alignment->second[SUPPLEMENTARY].end : chimeric_alignment->second[SUPPLEMENTARY].start;
+			direction_t direction_supplementary = (chimeric_alignment->second[SUPPLEMENTARY].strand == FORWARD) ? DOWNSTREAM : UPSTREAM;
+			position_t gap_start_mate1 = (chimeric_alignment->second[MATE1].strand == FORWARD) ? chimeric_alignment->second[MATE1].end : chimeric_alignment->second[MATE1].start;
+			direction_t direction_gap_start_mate1 = (chimeric_alignment->second[MATE1].strand == FORWARD) ? DOWNSTREAM : UPSTREAM;
+			position_t gap_start_split_read = (chimeric_alignment->second[SPLIT_READ].strand == FORWARD) ? chimeric_alignment->second[SPLIT_READ].end : chimeric_alignment->second[SPLIT_READ].start;
+			direction_t direction_gap_start_split_read = (chimeric_alignment->second[SPLIT_READ].strand == FORWARD) ? DOWNSTREAM : UPSTREAM;
+			position_t start_mate1 = (chimeric_alignment->second[MATE1].strand == FORWARD) ? chimeric_alignment->second[MATE1].start : chimeric_alignment->second[MATE1].end;
+			direction_t direction_start_mate1 = (chimeric_alignment->second[MATE1].strand == FORWARD) ? UPSTREAM : DOWNSTREAM;
 			if (abs(breakpoint_supplementary - breakpoint_split_read) <= fragment_size ||
 			    abs(breakpoint_supplementary - gap_start_mate1) <= fragment_size ||
 			    abs(breakpoint_supplementary - start_mate1) <= fragment_size ||
 			    abs(breakpoint_supplementary - gap_start_split_read) <= fragment_size ||
-			    spliced_distance_too_short(i->second[SPLIT_READ].contig, breakpoint_supplementary, breakpoint_split_read, direction_supplementary, direction_split_read, common_genes, fragment_size, exon_annotation_index) ||
-			    spliced_distance_too_short(i->second[MATE1].contig, breakpoint_supplementary, gap_start_mate1, direction_supplementary, direction_gap_start_mate1, common_genes, fragment_size, exon_annotation_index) ||
-			    spliced_distance_too_short(i->second[MATE1].contig, breakpoint_supplementary, start_mate1, direction_supplementary, direction_start_mate1, common_genes, fragment_size, exon_annotation_index) ||
-			    spliced_distance_too_short(i->second[SPLIT_READ].contig, breakpoint_supplementary, gap_start_split_read, direction_supplementary, direction_gap_start_split_read, common_genes, fragment_size, exon_annotation_index) ||
-			    is_breakpoint_within_aligned_segment(breakpoint_split_read, i->second[SUPPLEMENTARY]) ||
-			    is_breakpoint_within_aligned_segment(breakpoint_supplementary, i->second[SPLIT_READ]) ||
-			    is_breakpoint_within_aligned_segment(breakpoint_supplementary, i->second[MATE1])) {
-				i->second.filter = FILTERS.at("hairpin");
+			    spliced_distance_too_short(chimeric_alignment->second[SPLIT_READ].contig, breakpoint_supplementary, breakpoint_split_read, direction_supplementary, direction_split_read, common_genes, fragment_size, exon_annotation_index) ||
+			    spliced_distance_too_short(chimeric_alignment->second[MATE1].contig, breakpoint_supplementary, gap_start_mate1, direction_supplementary, direction_gap_start_mate1, common_genes, fragment_size, exon_annotation_index) ||
+			    spliced_distance_too_short(chimeric_alignment->second[MATE1].contig, breakpoint_supplementary, start_mate1, direction_supplementary, direction_start_mate1, common_genes, fragment_size, exon_annotation_index) ||
+			    spliced_distance_too_short(chimeric_alignment->second[SPLIT_READ].contig, breakpoint_supplementary, gap_start_split_read, direction_supplementary, direction_gap_start_split_read, common_genes, fragment_size, exon_annotation_index) ||
+			    is_breakpoint_within_aligned_segment(breakpoint_split_read, chimeric_alignment->second[SUPPLEMENTARY]) ||
+			    is_breakpoint_within_aligned_segment(breakpoint_supplementary, chimeric_alignment->second[SPLIT_READ]) ||
+			    is_breakpoint_within_aligned_segment(breakpoint_supplementary, chimeric_alignment->second[MATE1])) {
+				chimeric_alignment->second.filter = FILTERS.at("hairpin");
 				continue;
 			}
 
