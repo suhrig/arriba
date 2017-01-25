1a772a564cea2b2141206e837e11071a9bf72f9b
Sebastian Uhrig <s.uhrig@dkfz.de>
2017-01-25 10:20:41 +0100

    'end_to_end' filter does not remove fusions with spliced breakpoints

--- source/filter_end_to_end.cpp
+++ source/filter_end_to_end.cpp
@@ -6,17 +6,24 @@ using namespace std;
 unsigned int filter_end_to_end_fusions(fusions_t& fusions) {
 
 	unsigned int remaining = 0;
-	for (fusions_t::iterator i = fusions.begin(); i != fusions.end(); ++i) {
-		if (i->second.filter != NULL)
+	for (fusions_t::iterator fusion = fusions.begin(); fusion != fusions.end(); ++fusion) {
+
+		if (fusion->second.filter != NULL)
 			continue; // fusion has already been filtered
 
-		if (i->second.discordant_mates + i->second.split_reads1 == 0 || // only filter breakpoints with low support
-		    i->second.discordant_mates + i->second.split_reads2 == 0 ||
-		    i->second.split_reads1 + i->second.split_reads2 == 0 ||
-		    i->second.breakpoint_overlaps_both_genes() && (i->second.split_reads1 == 0 || i->second.split_reads2 == 0)) {
-			if ((i->second.gene1->is_dummy || (i->second.gene1->strand == FORWARD && i->second.direction1 == UPSTREAM) || (i->second.gene1->strand == REVERSE && i->second.direction1 == DOWNSTREAM)) &&
-			    (i->second.gene2->is_dummy || (i->second.gene2->strand == FORWARD && i->second.direction2 == UPSTREAM) || (i->second.gene2->strand == REVERSE && i->second.direction2 == DOWNSTREAM))) {
-				i->second.filter = FILTERS.at("end_to_end");
+		if (!fusion->second.is_read_through() &&
+		    (fusion->second.spliced1 || fusion->second.spliced2)) { // spliced breakpoints are likely true
+			++remaining;
+			continue;
+		}
+
+		if (fusion->second.discordant_mates + fusion->second.split_reads1 == 0 || // only filter breakpoints with low support
+		    fusion->second.discordant_mates + fusion->second.split_reads2 == 0 ||
+		    fusion->second.split_reads1 + fusion->second.split_reads2 == 0 ||
+		    fusion->second.breakpoint_overlaps_both_genes() && (fusion->second.split_reads1 == 0 || fusion->second.split_reads2 == 0)) {
+			if ((fusion->second.gene1->is_dummy || (fusion->second.gene1->strand == FORWARD && fusion->second.direction1 == UPSTREAM) || (fusion->second.gene1->strand == REVERSE && fusion->second.direction1 == DOWNSTREAM)) &&
+			    (fusion->second.gene2->is_dummy || (fusion->second.gene2->strand == FORWARD && fusion->second.direction2 == UPSTREAM) || (fusion->second.gene2->strand == REVERSE && fusion->second.direction2 == DOWNSTREAM))) {
+				fusion->second.filter = FILTERS.at("end_to_end");
 				continue;
 			}
 		}
