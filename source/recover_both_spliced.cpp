aff7d0b66942b1f9471bc6a01f643b0435faa9a0
Sebastian Uhrig <s.uhrig@dkfz.de>
2017-05-16 11:32:02 +0200

    'both_spliced' filter considers reciprocal translocations

--- source/recover_both_spliced.cpp
+++ source/recover_both_spliced.cpp
@@ -6,16 +6,27 @@
 
 using namespace std;
 
+bool both_spliced(const fusion_t& fusion) {
+	return fusion.spliced1 && fusion.spliced2 &&
+	       (fusion.gene1->strand == fusion.gene2->strand && fusion.direction1 != fusion.direction2 ||
+	        fusion.gene1->strand != fusion.gene2->strand && fusion.direction1 == fusion.direction2);
+}
+
+direction_t opposite_direction(const direction_t direction) {
+	return ((direction == DOWNSTREAM) ? UPSTREAM : DOWNSTREAM);
+}
+
 unsigned int recover_both_spliced(fusions_t& fusions, const unsigned int max_fusions_to_recover) {
 
 	// look for any supporting reads between two genes
 	map< tuple<gene_t,gene_t,direction_t,direction_t>, vector<fusion_t*> > fusions_by_gene_pair;
-	for (fusions_t::iterator i = fusions.begin(); i != fusions.end(); ++i)
-		if (i->second.filter == NULL ||
-		    i->second.filter == FILTERS.at("intronic") ||
-		    i->second.filter == FILTERS.at("promiscuous_genes") ||
-		    i->second.filter == FILTERS.at("min_support")) {
-			fusions_by_gene_pair[make_tuple(i->second.gene1, i->second.gene2, i->second.direction1, i->second.direction2)].push_back(&i->second);
+	for (fusions_t::iterator fusion = fusions.begin(); fusion != fusions.end(); ++fusion)
+		if (fusion->second.filter == NULL ||
+		    (both_spliced(fusion->second) && fusion->second.filter != FILTERS.at("merge_adjacent")) ||
+		    fusion->second.filter == FILTERS.at("intronic") ||
+		    fusion->second.filter == FILTERS.at("promiscuous_genes") ||
+		    fusion->second.filter == FILTERS.at("min_support")) {
+			fusions_by_gene_pair[make_tuple(fusion->second.gene1, fusion->second.gene2, fusion->second.direction1, fusion->second.direction2)].push_back(&fusion->second);
 		}
 
 	unsigned int remaining = 0;
@@ -40,33 +51,42 @@ unsigned int recover_both_spliced(fusions_t& fusions, const unsigned int max_fus
 			    fusion->second.filter != FILTERS.at("min_support"))
 				continue; // we won't recover fusions which were not discarded due to low support
 
-			if (!fusion->second.spliced1 || !fusion->second.spliced2)
-				continue; // both sides must be spliced
-
-			if (fusion->second.gene1->strand == fusion->second.gene2->strand && fusion->second.direction1 == fusion->second.direction2 ||
-			    fusion->second.gene1->strand != fusion->second.gene2->strand && fusion->second.direction1 != fusion->second.direction2)
-				continue; // exons would be spliced in a nonsensical way
+			if (!both_spliced(fusion->second))
+				continue; // only recover fusions with two spliced breakpoints
 
 			// count all supporting reads of all fusions between the pair of genes
+			unsigned int sum_of_supporting_reads = 0;
+
+			// look for other reads with the same orientation
 			auto fusions_of_given_gene_pair = fusions_by_gene_pair.find(make_tuple(fusion->second.gene1, fusion->second.gene2, fusion->second.direction1, fusion->second.direction2));
-			if (fusions_of_given_gene_pair != fusions_by_gene_pair.end()) {
-				unsigned int sum_of_supporting_reads = 0;
+			if (fusions_of_given_gene_pair != fusions_by_gene_pair.end())
 				for (auto another_fusion = fusions_of_given_gene_pair->second.begin(); another_fusion != fusions_of_given_gene_pair->second.end(); ++another_fusion)
 					sum_of_supporting_reads += max((unsigned int) 1, (**another_fusion).supporting_reads());
 
-				if (sum_of_supporting_reads >= 2) { // require at least two reads or else the false positive rate sky-rockets
-					if (mode == MODE_RECOVER) { // we are in recover mode => actually recover the fusion by clearing the filters
-						if (fusion->second.supporting_reads() >= min_supporting_reads && !fusion->second.is_read_through()) {
-							fusion->second.filter = NULL;
-							remaining++;
-						}
-					} else { // mode == MODE_COUNTING
-						// We are in dry-run mode, where we only count how many fusions would be recovered.
-						// If too many would be recovered, we increase the minimum required supporting reads,
-						// because spliced transcripts might simply be an artifact of the RNA extraction protocol.
-						unsigned int stratum = pow(2, ceil(log2(max((unsigned int) 1, fusion->second.supporting_reads()))));
-						recovered_fusions_by_supporting_reads[stratum]++;
+			// consider reciprocal translocations
+			auto reciprocal_fusions_of_given_gene_pair = fusions_by_gene_pair.find(make_tuple(fusion->second.gene1, fusion->second.gene2, opposite_direction(fusion->second.direction1), opposite_direction(fusion->second.direction2)));
+			if (reciprocal_fusions_of_given_gene_pair != fusions_by_gene_pair.end())
+				for (auto another_fusion = reciprocal_fusions_of_given_gene_pair->second.begin(); another_fusion != reciprocal_fusions_of_given_gene_pair->second.end(); ++another_fusion)
+					if (!(**another_fusion).is_read_through())
+						// if the fusion is supported by 2 event of which all breakpoints are spliced, we don't question it
+						// if the other fusion is not spliced, then we check if the reciprocal fusions support a common genomic breakpoint
+						if (both_spliced(**another_fusion) ||
+						    (((fusion->second.direction1 == DOWNSTREAM) != /*xor*/ (fusion->second.breakpoint1 > (**another_fusion).breakpoint1)) &&
+						     ((fusion->second.direction2 == DOWNSTREAM) != /*xor*/ (fusion->second.breakpoint2 > (**another_fusion).breakpoint2))))
+							sum_of_supporting_reads += max((unsigned int) 1, (**another_fusion).supporting_reads());
+
+			if (sum_of_supporting_reads >= 2) { // require at least two reads or else the false positive rate sky-rockets
+				if (mode == MODE_RECOVER) { // we are in recover mode => actually recover the fusion by clearing the filters
+					if (fusion->second.supporting_reads() >= min_supporting_reads && !fusion->second.is_read_through() && !fusion->second.breakpoint_overlaps_both_genes()) {
+						fusion->second.filter = NULL;
+						remaining++;
 					}
+				} else { // mode == MODE_COUNTING
+					// We are in dry-run mode, where we only count how many fusions would be recovered.
+					// If too many would be recovered, we increase the minimum required supporting reads,
+					// because spliced transcripts might simply be an artifact of the RNA extraction protocol.
+					unsigned int stratum = pow(2, ceil(log2(max((unsigned int) 1, fusion->second.supporting_reads()))));
+					recovered_fusions_by_supporting_reads[stratum]++;
 				}
 			}
 		}
