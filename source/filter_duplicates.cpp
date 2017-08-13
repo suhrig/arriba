d27339efcbb965c814a5ad1671a85c2c79991705
Sebastian Uhrig <s.uhrig@dkfz.de>
2017-08-13 16:40:44 +0200

    use supplementary alignment for marking of duplicate split reads

--- source/filter_duplicates.cpp
+++ source/filter_duplicates.cpp
@@ -8,16 +8,18 @@ using namespace std;
 unsigned int filter_duplicates(chimeric_alignments_t& chimeric_alignments) {
 	unsigned int remaining = 0;
 	unordered_map< tuple<position_t,position_t,position_t,position_t> , unsigned int> duplicate_count;
-	for (chimeric_alignments_t::iterator i = chimeric_alignments.begin(); i != chimeric_alignments.end(); ++i) {
-		if (i->second.filter != NULL)
+	for (chimeric_alignments_t::iterator chimeric_alignment = chimeric_alignments.begin(); chimeric_alignment != chimeric_alignments.end(); ++chimeric_alignment) {
+		if (chimeric_alignment->second.filter != NULL)
 			continue; // read has already been filtered
 
-		tuple<position_t,position_t,position_t,position_t> mate_coordinates = make_tuple(i->second[MATE1].start - i->second[MATE1].preclipping(),
-												 i->second[MATE1].end   + i->second[MATE1].postclipping(),
-												 i->second[MATE2].start - i->second[MATE2].preclipping(),
-												 i->second[MATE2].end   + i->second[MATE2].postclipping());
+		unsigned int mate2 = (chimeric_alignment->second.size() == 2) ? MATE2 : SUPPLEMENTARY;
+
+		tuple<position_t,position_t,position_t,position_t> mate_coordinates = make_tuple(chimeric_alignment->second[MATE1].start - chimeric_alignment->second[MATE1].preclipping(),
+												 chimeric_alignment->second[MATE1].end   + chimeric_alignment->second[MATE1].postclipping(),
+												 chimeric_alignment->second[mate2].start - chimeric_alignment->second[mate2].preclipping(),
+												 chimeric_alignment->second[mate2].end   + chimeric_alignment->second[mate2].postclipping());
 		if (duplicate_count[mate_coordinates]++ > 0)
-			i->second.filter = FILTERS.at("duplicates");
+			chimeric_alignment->second.filter = FILTERS.at("duplicates");
 		else
 			++remaining;
 	}
