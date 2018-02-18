c3206828a980bd69d2ba877dd1ed16ac2096b4b5
Sebastian Uhrig <s.uhrig@dkfz.de>
2018-02-18 19:03:10 +0100

    draw narrow bands for non-coding regions

--- draw_fusions.R
+++ draw_fusions.R
@@ -55,11 +55,11 @@ if (nrow(fusions) == 0) {
 # read exon annotation
 exons <- read.table(exonsFile, header=F, sep="\t", comment.char="#", quote="", stringsAsFactors=F)[,c(1, 3, 4, 5, 7, 9)]
 colnames(exons) <- c("contig", "type", "start", "end", "strand", "attributes")
-exons <- exons[exons$type %in% c("exon", "UTR") & grepl("exon_number ", exons$attributes),]
+exons <- exons[exons$type %in% c("exon", "CDS") & grepl("exon_number ", exons$attributes),]
 exons$geneName <- gsub(".*gene_name \"?([^;\"]+)\"?;.*", "\\1", exons$attributes)
 exons$transcript <- gsub(".*transcript_id \"?([^;\"]+)\"?;.*", "\\1", exons$attributes)
 exons$exonNumber <- gsub(".*exon_number \"?([^;\"]+)\"?;.*", "\\1", exons$attributes)
-exons <- exons[, !colnames(exons) %in% c("type", "attributes")]
+exons <- exons[, !colnames(exons) %in% c("attributes")]
 
 # insert dummy annotations for dummy genes
 if (any(grepl(",", fusions$gene1) | grepl(",", fusions$gene2))) {
@@ -69,6 +69,7 @@ if (any(grepl(",", fusions$gene1) | grepl(",", fusions$gene2))) {
 	)
 	exons <- rbind(exons, data.frame(
 		contig=intergenicBreakpoints$contig,
+		type="exon",
 		start=intergenicBreakpoints$breakpoint-5000,
 		end=intergenicBreakpoints$breakpoint+5000,
 		strand="",
@@ -92,6 +93,19 @@ drawStrands <- function(start, end, y, col, strand, scalingFactor, drawLabels=T,
 	if (strand %in% c("+", "-")) {
 		for (i in seq(start+scalingFactor*0.01, end-scalingFactor*0.01, by=sign(end-start-2*scalingFactor*0.01)*scalingFactor*0.01))
 			arrows(i, y, i+scalingFactor*0.001*ifelse(strand=="+", 1, -1), y, col=col, length=0.08, lwd=2, angle=60)
+	}
+}
+
+drawExon <- function(start, end, y, color, title, type) {
+	if (title != "dummy") {
+		if (type == "CDS") {
+			# draw coding regions as thicker bars
+			rect(start, y+0.05, end, y+0.025, col=color, border=NA)
+			rect(start, y-0.05, end, y-0.025, col=color, border=NA)
+		} else {
+			rect(start, y+0.025, end, y-0.025, col=color, border=NA)
+			text((start+end)/2, y, title, cex=0.75)
+		}
 	}
 }
 
@@ -119,14 +133,14 @@ for (fusion in 1:nrow(fusions)) {
 	breakpoint1 <- fusions[fusion,"breakpoint1"]
 	breakpoint2 <- fusions[fusion,"breakpoint2"]
 	if (breakpoint1 < min(exons1$start)) {
-		exons1 <- rbind(c(exons1[1,"transcript"], exons1[1,"contig"], breakpoint1-1000, breakpoint1-1000, exons1[1,"strand"], "dummy", "", exons1[1,"transcriptStart"], exons1[1,"transcriptEnd"], exons1[1,"count"]), exons1)
+		exons1 <- rbind(c(exons1[1,"transcript"], exons1[1,"contig"], "dummy", breakpoint1-1000, breakpoint1-1000, exons1[1,"strand"], "dummy", "", exons1[1,"transcriptStart"], exons1[1,"transcriptEnd"], exons1[1,"count"]), exons1)
 	} else if (breakpoint1 > max(exons1$end)) {
-		exons1 <- rbind(exons1, c(exons1[1,"transcript"], exons1[1,"contig"], breakpoint1+1000, breakpoint1+1000, exons1[1,"strand"], "dummy", "", exons1[1,"transcriptStart"], exons1[1,"transcriptEnd"], exons1[1,"count"]))
+		exons1 <- rbind(exons1, c(exons1[1,"transcript"], exons1[1,"contig"], "dummy", breakpoint1+1000, breakpoint1+1000, exons1[1,"strand"], "dummy", "", exons1[1,"transcriptStart"], exons1[1,"transcriptEnd"], exons1[1,"count"]))
 	}
 	if (breakpoint2 < min(exons2$start)) {
-		exons2 <- rbind(c(exons2[1,"transcript"], exons2[1,"contig"], breakpoint2-1000, breakpoint2-1000, exons2[1,"strand"], "dummy", "", exons2[1,"transcriptStart"], exons2[1,"transcriptEnd"], exons2[1,"count"]), exons2)
+		exons2 <- rbind(c(exons2[1,"transcript"], exons2[1,"contig"], "dummy", breakpoint2-1000, breakpoint2-1000, exons2[1,"strand"], "dummy", "", exons2[1,"transcriptStart"], exons2[1,"transcriptEnd"], exons2[1,"count"]), exons2)
 	} else if (breakpoint2 > max(exons2$end)) {
-		exons2 <- rbind(exons2, c(exons2[1,"transcript"], exons2[1,"contig"], breakpoint2+1000, breakpoint2+1000, exons2[1,"strand"], "dummy", "", exons2[1,"transcriptStart"], exons2[1,"transcriptEnd"], exons2[1,"count"]))
+		exons2 <- rbind(exons2, c(exons2[1,"transcript"], exons2[1,"contig"], "dummy", breakpoint2+1000, breakpoint2+1000, exons2[1,"strand"], "dummy", "", exons2[1,"transcriptStart"], exons2[1,"transcriptEnd"], exons2[1,"count"]))
 	}
 	exons1$start <- as.integer(exons1$start)
 	exons1$end <- as.integer(exons1$end)
@@ -140,7 +154,8 @@ for (fusion in 1:nrow(fusions)) {
 		for (exon in 1:nrow(exons1)) {
 			if (breakpoint1 > previousExonEnd+1 && breakpoint1 < exons1[exon,"start"])
 				breakpoint1 <- (breakpoint1-previousExonEnd) / (exons1[exon,"start"]-previousExonEnd) * 200 + previousExonEnd - cumulativeIntronLength
-			cumulativeIntronLength <- cumulativeIntronLength + exons1[exon,"start"] - previousExonEnd - 200
+			if (exons1[exon,"start"] > previousExonEnd)
+				cumulativeIntronLength <- cumulativeIntronLength + exons1[exon,"start"] - previousExonEnd - 200
 			if (breakpoint1 >= exons1[exon,"start"] && breakpoint1 <= exons1[exon,"end"]+1)
 				breakpoint1 <- breakpoint1 - cumulativeIntronLength
 			previousExonEnd <- exons1[exon,"end"]
@@ -154,7 +169,8 @@ for (fusion in 1:nrow(fusions)) {
 		for (exon in 1:nrow(exons2)) {
 			if (breakpoint2 > previousExonEnd+1 && breakpoint2 < exons2[exon,"start"])
 				breakpoint2 <- (breakpoint2-previousExonEnd) / (exons2[exon,"start"]-previousExonEnd) * 200 + previousExonEnd - cumulativeIntronLength
-			cumulativeIntronLength <- cumulativeIntronLength + exons2[exon,"start"] - previousExonEnd - 200
+			if (exons2[exon,"start"] > previousExonEnd)
+				cumulativeIntronLength <- cumulativeIntronLength + exons2[exon,"start"] - previousExonEnd - 200
 			if (breakpoint2 >= exons2[exon,"start"] && breakpoint2 <= exons2[exon,"end"]+1)
 				breakpoint2 <- breakpoint2 - cumulativeIntronLength
 			previousExonEnd <- exons2[exon,"end"]
@@ -178,23 +194,13 @@ for (fusion in 1:nrow(fusions)) {
 
 	# plot gene 1 (at top of the page)
 	drawStrands(-0.02*scalingFactor, max(exons1$end)+0.02*scalingFactor, 0.85, "darkolivegreen4", exons1[1,"strand"], scalingFactor, gene=fusions[fusion,"gene1"])
-	for (exon in 1:nrow(exons1)) {
-		if (exons1[exon,"exonNumber"] != "dummy") {
-			rect(exons1[exon,"start"], 0.9, exons1[exon,"end"], 0.8, col="darkolivegreen2", border=NA)
-			y <- ifelse(exon %% 2, 0.875, 0.825)
-			text((exons1[exon,"end"]+exons1[exon,"start"])/2, y, exons1[exon,"exonNumber"], cex=0.75)
-		}
-	}
+	for (exon in 1:nrow(exons1))
+		drawExon(exons1[exon,"start"], exons1[exon,"end"], 0.85, "darkolivegreen2", exons1[exon,"exonNumber"], exons1[exon,"type"])
 
 	# plot gene 2 (at the bottom of the page)
 	drawStrands(-0.02*scalingFactor, max(exons2$end)+0.02*scalingFactor, 0.15, col="deepskyblue4", exons2[1,"strand"], scalingFactor, gene=fusions[fusion,"gene2"])
-	for (exon in 1:nrow(exons2)) {
-		if (exons2[exon,"exonNumber"] != "dummy") {
-			rect(exons2[exon,"start"], 0.1, exons2[exon,"end"], 0.2, col="deepskyblue2", border=NA)
-			y <- ifelse(exon %% 2, 0.175, 0.125)
-			text((exons2[exon,"end"]+exons2[exon,"start"])/2, y, exons2[exon,"exonNumber"], cex=0.75)
-		}
-	}
+	for (exon in 1:nrow(exons2))
+		drawExon(exons2[exon,"start"], exons2[exon,"end"], 0.15, "deepskyblue2", exons2[exon,"exonNumber"], exons2[exon,"type"])
 	
 	fusionOffset <- ifelse(fusions[fusion,"direction1"] == "downstream", breakpoint1, max(exons1$end)-breakpoint1)
 	
@@ -204,11 +210,8 @@ for (fusion in 1:nrow(fusions)) {
 		drawStrands(-0.02*scalingFactor, breakpoint1, 0.5, col="darkolivegreen4", exons1[1,"strand"], scalingFactor, drawLabels=F, gene="fusion")
 		# plot exons
 		for (exon in 1:nrow(exons1))
-			if (exons1[exon,"start"] <= breakpoint1 && exons1[exon,"exonNumber"] != "dummy") {
-				rect(exons1[exon,"start"], 0.45, min(breakpoint1, exons1[exon,"end"]), 0.55, col="darkolivegreen2", border=NA)
-				y <- ifelse(exon %% 2, 0.475, 0.525)
-				text((exons1[exon,"start"]+min(breakpoint1, exons1[exon,"end"]))/2, y, exons1[exon,"exonNumber"], cex=0.75)
-			}
+			if (exons1[exon,"start"] <= breakpoint1)
+				drawExon(exons1[exon,"start"], min(breakpoint1, exons1[exon,"end"]), 0.5, "darkolivegreen2", exons1[exon,"exonNumber"], exons1[exon,"type"])
 		# plot trajectories
 		lines(c(breakpoint1, breakpoint1), c(0.8, 0.55), col="red", lty=2)
 		lines(c(0, 0), c(0.8, 0.55), col="red", lty=2)
@@ -218,11 +221,8 @@ for (fusion in 1:nrow(fusions)) {
 		drawStrands(-0.02*scalingFactor, fusionOffset, 0.5, col="darkolivegreen4", chartr("+-", "-+", exons1[1,"strand"]), scalingFactor, gene="fusion", drawLabels=F)
 		# plot exons
 		for (exon in 1:nrow(exons1))
-			if (exons1[exon,"end"]+1 >= breakpoint1 && exons1[exon,"exonNumber"] != "dummy") {
-				rect(max(exons1$end)-exons1[exon,"end"], 0.45, min(fusionOffset, max(exons1$end)-exons1[exon,"start"]), 0.55, col="darkolivegreen2", border=NA)
-				y <- ifelse(exon %% 2, 0.475, 0.525)
-				text((max(exons1$end)-exons1[exon,"end"]+min(fusionOffset, max(exons1$end)-exons1[exon,"start"]))/2, y, exons1[exon,"exonNumber"], cex=0.75)
-			}
+			if (exons1[exon,"end"]+1 >= breakpoint1)
+				drawExon(max(exons1$end)-exons1[exon,"end"], min(fusionOffset, max(exons1$end)-exons1[exon,"start"]), 0.5, "darkolivegreen2", exons1[exon,"exonNumber"], exons1[exon,"type"])
 		# plot trajectories
 		lines(c(breakpoint1, fusionOffset), c(0.8, 0.55), col="red", lty=2)
 		lines(c(max(exons1$end), 0), c(0.8, 0.55), col="red", lty=2)
@@ -238,11 +238,8 @@ for (fusion in 1:nrow(fusions)) {
 		drawStrands(fusionOffset, fusionOffset+breakpoint2+0.02*scalingFactor, 0.5, col="deepskyblue4", chartr("+-", "-+", exons2[1,"strand"]), scalingFactor, drawLabels=F)
 		# plot exons
 		for (exon in 1:nrow(exons2))
-			if (exons2[exon,"start"] <= breakpoint2 && exons2[exon,"exonNumber"] != "dummy") {
-				rect(max(fusionOffset, fusionOffset+breakpoint2-exons2[exon,"end"]), 0.45, fusionOffset+breakpoint2-exons2[exon,"start"], 0.55, col="deepskyblue2", border=NA)
-				y <- ifelse(exon %% 2, 0.475, 0.525)
-				text((max(fusionOffset, fusionOffset+breakpoint2-exons2[exon,"end"])+fusionOffset+breakpoint2-exons2[exon,"start"])/2, y, exons2[exon,"exonNumber"], cex=0.75)
-			}
+			if (exons2[exon,"start"] <= breakpoint2)
+				drawExon(max(fusionOffset, fusionOffset+breakpoint2-exons2[exon,"end"]), fusionOffset+breakpoint2-exons2[exon,"start"], 0.5, "deepskyblue2", exons2[exon,"exonNumber"], exons2[exon,"type"])
 		# plot trajectories
 		lines(c(breakpoint2, fusionOffset), c(0.2, 0.45), col="red", lty=2)
 		lines(c(0, fusionOffset+breakpoint2), c(0.2, 0.45), col="red", lty=2)
@@ -252,11 +249,8 @@ for (fusion in 1:nrow(fusions)) {
 		drawStrands(fusionOffset, fusionOffset+max(exons2$end)-breakpoint2+0.02*scalingFactor, 0.5, col="deepskyblue4", exons2[1,"strand"], scalingFactor, drawLabels=F)
 		# plot exons
 		for (exon in 1:nrow(exons2))
-			if (exons2[exon,"end"]+1 >= breakpoint2 && exons2[exon,"exonNumber"] != "dummy") {
-				rect(max(fusionOffset, fusionOffset+exons2[exon,"start"]-breakpoint2), 0.45, fusionOffset+exons2[exon,"end"]-breakpoint2, 0.55, col="deepskyblue2", border=NA)
-				y <- ifelse(exon %% 2, 0.475, 0.525)
-				text((max(fusionOffset, fusionOffset+exons2[exon,"start"]-breakpoint2)+fusionOffset+exons2[exon,"end"]-breakpoint2)/2, y, exons2[exon,"exonNumber"], cex=0.75)
-			}
+			if (exons2[exon,"end"]+1 >= breakpoint2)
+				drawExon(max(fusionOffset, fusionOffset+exons2[exon,"start"]-breakpoint2), fusionOffset+exons2[exon,"end"]-breakpoint2, 0.5, "deepskyblue2", exons2[exon,"exonNumber"], exons2[exon,"type"])
 		# plot trajectories
 		lines(c(breakpoint2, fusionOffset), c(0.2, 0.45), col="red", lty=2)
 		lines(c(max(exons2$end), fusionOffset+max(exons2$end)-breakpoint2), c(0.2, 0.45), col="red", lty=2)
