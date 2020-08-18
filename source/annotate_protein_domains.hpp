e3c2b94befaf6c85b839bb58e4e0a8c8080f6c27
Sebastian Uhrig <s.uhrig@dkfz.de>
2020-08-18 21:30:51 +0200

    move protein-related functions

--- source/annotate_protein_domains.hpp
+++ source/annotate_protein_domains.hpp
@@ -3,6 +3,8 @@
 
 #include <string>
 #include <unordered_map>
+#include <vector>
+#include <unordered_map>
 #include "common.hpp"
 
 using namespace std;
@@ -19,5 +21,13 @@ typedef annotation_index_t<protein_domain_t> protein_domain_annotation_index_t;
 void load_protein_domains(const string& filename, const contigs_t& contigs, const gene_annotation_t& gene_annotation, const unordered_map<string,gene_t>& gene_names, protein_domain_annotation_t& protein_domain_annotation, protein_domain_annotation_index_t& protein_domain_annotation_index);
 
 string annotate_retained_protein_domains(const contig_t contig, const position_t breakpoint, const strand_t predicted_strand, const bool predicted_strand_ambiguous, const gene_t gene, const direction_t direction, const protein_domain_annotation_index_t& protein_domain_annotation_index);
+
+char dna_to_protein(const string& triplet);
+
+int get_reading_frame(const vector<position_t>& transcribed_bases, const int from, const int to, const transcript_t transcript, const gene_t gene, const assembly_t& assembly, exon_t& exon_with_start_codon);
+
+string get_fusion_peptide_sequence(const string& transcript_sequence, const vector<position_t>& positions, const gene_t gene_5, const gene_t gene_3, const transcript_t transcript_5, const transcript_t transcript_3, const strand_t predicted_strand_3, const exon_annotation_index_t& exon_annotation_index, const assembly_t& assembly);
+
+string is_in_frame(const string& fusion_peptide_sequence);
 
 #endif /* _ANNOTATE_PROTEIN_DOMAINS_H */
