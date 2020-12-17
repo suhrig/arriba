b61e86568580fec597f0ffc94f0f946116bb9f1e
Sebastian Uhrig <s.uhrig@dkfz.de>
2020-12-17 22:54:54 +0100

    speed up loading of input files

--- draw_fusions.R
+++ draw_fusions.R
@@ -113,7 +113,7 @@ addChr <- function(contig) {
 	ifelse(contig == "MT", "chrM", paste0("chr", contig))
 }
 removeChr <- function(contig) {
-	sub("^chr", "", sub("^chrM", "MT", contig), perl=T)
+	sub("^chr", "", sub("^chrM", "MT", contig, perl=T), perl=T)
 }
 
 # convenience function to check if a value is between two others
@@ -132,7 +132,7 @@ if (colnames(fusions)[1] == "X.gene1") { # Arriba output
 	fusions$breakpoint1 <- as.numeric(sub(".*:", "", fusions$breakpoint1, perl=T))
 	fusions$breakpoint2 <- as.numeric(sub(".*:", "", fusions$breakpoint2, perl=T))
 	fusions$split_reads <- fusions$split_reads1 + fusions$split_reads2
-	fusions$type <- sub(".*(translocation|duplication|deletion|inversion).*", "\\1", fusions$type)
+	fusions$type <- sub(".*(translocation|duplication|deletion|inversion).*", "\\1", fusions$type, perl=T)
 } else if (colnames(fusions)[1] == "X.FusionName") { # STAR-Fusion
 	fusions$gene1 <- sub("\\^.*", "", fusions$LeftGene, perl=T)
 	fusions$gene2 <- sub("\\^.*", "", fusions$RightGene, perl=T)
@@ -148,7 +148,7 @@ if (colnames(fusions)[1] == "X.gene1") { # Arriba output
 	fusions$direction2 <- ifelse(grepl(":\\+$", fusions$RightBreakpoint, perl=T), "upstream", "downstream")
 	fusions$transcript_id1 <- ifelse(rep(!("CDS_LEFT_ID" %in% colnames(fusions)), nrow(fusions)), ".", fusions$CDS_LEFT_ID)
 	fusions$transcript_id2 <- ifelse(rep(!("CDS_RIGHT_ID" %in% colnames(fusions)), nrow(fusions)), ".", fusions$CDS_RIGHT_ID)
-	fusions$fusion_transcript <- ifelse(rep(!("FUSION_CDS" %in% colnames(fusions)), nrow(fusions)), ".", toupper(sub("([a-z]*)", "\\1|", fusions$FUSION_CDS)))
+	fusions$fusion_transcript <- ifelse(rep(!("FUSION_CDS" %in% colnames(fusions)), nrow(fusions)), ".", toupper(sub("([a-z]*)", "\\1|", fusions$FUSION_CDS, perl=T)))
 	fusions$reading_frame <- ifelse(rep(!("PROT_FUSION_TYPE" %in% colnames(fusions)), nrow(fusions)), ".", ifelse(fusions$PROT_FUSION_TYPE == "INFRAME", "in-frame", ifelse(fusions$PROT_FUSION_TYPE == "FRAMESHIFT", "out-of-frame", ".")))
 	fusions$split_reads <- fusions$JunctionReadCount
 	fusions$discordant_mates <- fusions$SpanningFragCount
@@ -177,15 +177,16 @@ if (cytobandsFile != "") {
 
 # read exon annotation
 message("Loading annotation")
-exons <- read.table(exonsFile, header=F, sep="\t", comment.char="#", quote="", stringsAsFactors=F)[,c(1, 3, 4, 5, 7, 9)]
-colnames(exons) <- c("contig", "type", "start", "end", "strand", "attributes")
-exons <- exons[exons$type %in% c("exon", "CDS"),]
+exons <- scan(exonsFile, what=list(contig="",src="",type="",start=0,end=0,score="",strand="",frame="",attributes=""), sep="\t", comment.char="#", quote='"', multi.line=F)
+attr(exons, "row.names") <- .set_row_names(length(exons[[1]]))
+class(exons) <- "data.frame"
+exons <- exons[exons$type %in% c("exon","CDS"),c("contig","type","start","end","strand","attributes")]
 exons$contig <- removeChr(exons$contig)
-parseGtfAttribute <- function(attribute, exons) {
-	parsed <- gsub(paste0(".*", attribute, " \"?([^;\"]+)\"?;.*"), "\\1", exons$attributes)
-	failedToParse <- parsed == exons$attributes
+parseGtfAttribute <- function(attribute, gtf) {
+	parsed <- sub(paste0(".*", attribute, "[ =]([^;]+).*"), "\\1", gtf$attributes, perl=T)
+	failedToParse <- parsed == gtf$attributes
 	if (any(failedToParse)) {
-		warning(paste0("Failed to parse '", attribute, "' attribute of ", sum(failedToParse), " GTF record(s)."))
+		warning(paste0("Failed to parse '", attribute, "' attribute of ", sum(failedToParse), " record(s)."))
 		parsed <- ifelse(failedToParse, "", parsed)
 	}
 	return(parsed)
@@ -200,12 +201,12 @@ exons$exonNumber <- ifelse(rep(printExonLabels, nrow(exons)), parseGtfAttribute(
 proteinDomains <- NULL
 if (proteinDomainsFile != "") {
 	message("Loading protein domains")
-	proteinDomains <- read.table(proteinDomainsFile, header=F, sep="\t", comment.char="", quote="", stringsAsFactors=F)[,c(1,4,5,7,9)]
-	colnames(proteinDomains) <- c("contig", "start", "end", "strand", "attributes")
-	proteinDomains$color <- sub(";.*", "", sub(".*color=", "", proteinDomains$attributes, perl=T), perl=T)
-	proteinDomains$proteinDomainName <- sapply(sub(";.*", "", sub(".*Name=", "", proteinDomains$attributes, perl=T), perl=T), URLdecode)
-	proteinDomains$proteinDomainID <- sub(";.*", "", sub(".*protein_domain_id=", "", proteinDomains$attributes, perl=T), perl=T)
-	proteinDomains <- proteinDomains[,colnames(proteinDomains) != "attributes"]
+	proteinDomains <- scan(proteinDomainsFile, what=list(contig="",src="",type="",start=0,end=0,score="",strand="",frame="",attributes=""), sep="\t", comment.char="", quote="", multi.line=F)
+	attr(proteinDomains, "row.names") <- .set_row_names(length(proteinDomains[[1]]))
+	class(proteinDomains) <- "data.frame"
+	proteinDomains$color <- parseGtfAttribute("color", proteinDomains)
+	proteinDomains$proteinDomainName <- sapply(parseGtfAttribute("Name", proteinDomains), URLdecode)
+	proteinDomains$proteinDomainID <- parseGtfAttribute("protein_domain_id", proteinDomains)
 }
 
 # insert dummy annotations for intergenic breakpoints
@@ -1180,7 +1181,7 @@ for (fusion in 1:nrow(fusions)) {
 		fusion_transcript2 <- gsub(".*\\|", "", fusions[fusion,"fusion_transcript"], perl=T)
 		fusion_transcript2 <- substr(fusion_transcript2, 1, min(nchar(fusion_transcript2), 30))
 		# check for non-template bases
-		non_template_bases <- gsub(".*\\|([^|]*)\\|.*", "\\1", fusions[fusion,"fusion_transcript"])
+		non_template_bases <- gsub(".*\\|([^|]*)\\|.*", "\\1", fusions[fusion,"fusion_transcript"], perl=T)
 		if (non_template_bases == fusions[fusion,"fusion_transcript"]) # no non-template bases found
 			non_template_bases <- ""
 		# divide non-template bases half-and-half for centered alignment
@@ -1243,3 +1244,4 @@ for (fusion in 1:nrow(fusions)) {
 }
 
 devNull <- dev.off()
+message("Done")
