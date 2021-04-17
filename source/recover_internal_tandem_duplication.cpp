33e804170916ca0187d88048c9734394f9a6edbd
Sebastian Uhrig <s.uhrig@dkfz.de>
2021-04-17 15:42:25 +0200

    internal tandem duplications may protrude into introns a bit

--- source/recover_internal_tandem_duplication.cpp
+++ source/recover_internal_tandem_duplication.cpp
@@ -7,8 +7,9 @@ using namespace std;
 
 unsigned int recover_internal_tandem_duplication(fusions_t& fusions, const chimeric_alignments_t& chimeric_alignments, const coverage_t& coverage, const exon_annotation_index_t& exon_annotation_index, const unsigned int max_itd_length) {
 
-	unsigned int min_supporting_reads = 10; // only recover fusions with this many supporting reads
-	float min_fraction_of_coverage = 0.05; // the fusion-supporting reads must make up a substantial fraction of the coverage
+	const unsigned int min_supporting_reads = 10; // only recover fusions with this many supporting reads
+	const float min_fraction_of_coverage = 0.05; // the fusion-supporting reads must make up a substantial fraction of the coverage
+	const int protrude_into_introns = 7; // ITDs should be in exonic regions, but they may protrude into introns by this many bases
 
 	// estimate duplication rate, because the calculated coverage includes duplicates
 	unsigned int duplicates = 0;
@@ -40,8 +41,8 @@ unsigned int recover_internal_tandem_duplication(fusions_t& fusions, const chime
 			bool is_in_coding_region = false;
 			for (auto exon = exons.begin(); exon != exons.end(); ++exon)
 				if ((**exon).gene == fusion->second.gene1 &&
-				    (**exon).coding_region_start <= fusion->second.breakpoint1 && (**exon).coding_region_end >= fusion->second.breakpoint1 &&
-				    (**exon).coding_region_start <= fusion->second.breakpoint2 && (**exon).coding_region_end >= fusion->second.breakpoint2)
+				    (**exon).coding_region_start <= fusion->second.breakpoint1 + protrude_into_introns && (**exon).coding_region_end + protrude_into_introns >= fusion->second.breakpoint1 &&
+				    (**exon).coding_region_start <= fusion->second.breakpoint2 + protrude_into_introns && (**exon).coding_region_end + protrude_into_introns >= fusion->second.breakpoint2)
 					is_in_coding_region = true;
 			if (!is_in_coding_region)
 				continue;
