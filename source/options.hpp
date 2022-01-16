bbc4f0be9138e15e13bdc19dff3a9f07ca666bfe
Sebastian Uhrig <s.uhrig@dkfz.de>
2022-01-16 13:19:58 +0100

    Arriba 2.2.0

--- source/options.hpp
+++ source/options.hpp
@@ -12,7 +12,7 @@ const string HELP_CONTACT = "https://github.com/suhrig/arriba/issues";
 const string USER_MANUAL = "https://arriba.readthedocs.io/";
 const string CODE_REPOSITORY = "https://github.com/suhrig/arriba";
 const string CITATION = "https://doi.org/10.1101/gr.257246.119";
-const string ARRIBA_VERSION = "2.1.0";
+const string ARRIBA_VERSION = "2.2.0";
 
 string wrap_help(const string& option, const string& text, const unsigned short int max_line_width = 80);
 
